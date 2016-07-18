72d4b37d7049fdec800c5ca4b9f8ecbae78d5a71
Fahad Ibnay Heylaal <fahad19@gmail.com>
2016-07-18 11:35:35 +0200

    base setup.

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: [
+    './base',
+    './react'
+  ].map(require.resolve),
+  rules: {}
+};
---
+++ rules/node.js
@@ -0,0 +1,1 @@
+module.exports = require('eslint-config-airbnb-base/rules/node');
